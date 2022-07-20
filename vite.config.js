3f5ec9cc08944e1b53822537127ae4c449bd8c86
lulumarquezzini <marquezzini@dcc.ufrj.br>
2022-07-20 03:57:53 -0300

    fixing base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/coins-price/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
